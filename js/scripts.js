b641595e29aecbac986a7f7449bfa2aaa61b498b
Kelly Becker <kellywerks@gmail.com>
2015-05-28 09:05:20 -0700

    add initial files

---
+++ js/scripts.js
@@ -0,0 +1,38 @@
+$(document).ready(function() {
+  $("form#new-contact").submit(function(event) {
+    event.preventDefault();
+
+    var inputtedFirstName = $("input#new-first-name").val();
+    var inputtedLastName = $("input#new-last-name").val();
+    var inputtedStreet = $("input#new-street").val();
+    var inputtedCity = $("input#new-city").val();
+    var inputtedState = $("input#new-state").val();
+    var newAddress = { street: inputtedStreet,
+                       city: inputtedCity,
+                       state: inputtedState,
+                       fullAddress: function() {
+                         return this.street + ", " + this.city + ", " + this.state;
+                       }
+                     };
+    var newContact = { firstName: inputtedFirstName,
+                       lastName: inputtedLastName,
+                       address: newAddress.fullAddress()
+                      };
+
+    $("ul#contacts").append("<li><span class='contact'>" + newContact.firstName + " " + newContact.lastName + "</span></li>");
+
+    $("input#new-first-name").val("");
+    $("input#new-last-name").val("");
+    $("input#new-street").val("");
+    $("input#new-city").val("");
+    $("input#new-state").val("");
+
+    $(".contact").last().click(function() {
+      $("#show-contact").show();
+      $("#show-contact h2").text(newContact.firstName + " " + newContact.lastName);
+      $(".first-name").text(newContact.firstName);
+      $(".last-name").text(newContact.lastName);
+      $(".address").text(newContact.address);
+    });
+  });
+});
