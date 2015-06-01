11a0c82e909201e374a1812deba4be39a440d702
Kelly Becker <kellywerks@gmail.com>
2015-06-01 09:15:20 -0700

    update specs (empty)

--- spec/specs.js
+++
@@ -1,6 +0,0 @@
-describe('Dog', function() {
-  it("creates a new dog with the given specifications", function() {
-    var testDog = new Dog("Paco",["brown","white"],3);
-    expect(testDog.name).to.equal("Paco");
-  });
-});
