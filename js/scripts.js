37f1050f243287113397693851873ec7a805b512
Kelly Becker <kellywerks@gmail.com>
2015-06-01 09:11:44 -0700

    add final files for Address Book Part 1

--- js/scripts.js
+++ js/scripts.js
@@ -4,20 +4,21 @@ $(document).ready(function() {
 
     var inputtedFirstName = $("input#new-first-name").val();
     var inputtedLastName = $("input#new-last-name").val();
-    var inputtedStreet = $("input#new-street").val();
-    var inputtedCity = $("input#new-city").val();
-    var inputtedState = $("input#new-state").val();
-    var newContact = { firstName: inputtedFirstName,
-                       lastName: inputtedLastName,
-                       address: newAddress.fullAddress()
-                      };
+    var inputtedAddress = $("input#new-address").val();
+    var newContact = { firstName: inputtedFirstName, lastName: inputtedLastName, address: inputtedAddress };
 
     $("ul#contacts").append("<li><span class='contact'>" + newContact.firstName + " " + newContact.lastName + "</span></li>");
 
     $("input#new-first-name").val("");
     $("input#new-last-name").val("");
-    $("input#new-street").val("");
-    $("input#new-city").val("");
-    $("input#new-state").val("");
+    $("input#new-address").val("");
+
+    $(".contact").last().click(function() {
+      $("#show-contact").show();
+      $("#show-contact h2").text(newContact.firstName + " " + newContact.lastName);
+      $(".first-name").text(newContact.firstName);
+      $(".last-name").text(newContact.lastName);
+      $(".address").text(newContact.address);
+    });
   });
 });
